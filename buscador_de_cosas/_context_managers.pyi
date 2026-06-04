7177603359933947d2325ca459e7b6c99429c173
Adam Levasseur <letmeadam@gmail.com>
2026-06-04 14:20:45 -0400

    fix(stubs): Update Python stubs

--- buscador_de_cosas/_context_managers.pyi
+++ buscador_de_cosas/_context_managers.pyi
@@ -1,7 +1,12 @@
 import collections
+import contextlib
 import typing
 
-from Qt import QtCore, QtWidgets
+from Qt import QtCore, QtGui, QtWidgets
 
+@contextlib.contextmanager
 def disconnect_temporarily(callbacks_tuples: collections.abc.Iterable[tuple[QtCore.Signal, typing.Callable]]) -> typing.Generator[None, None, None]: ...
+@contextlib.contextmanager
 def block_signals(widgets: collections.abc.Iterable[QtWidgets.QWidget]) -> typing.Generator[None, None, None]: ...
+@contextlib.contextmanager
+def preserved_painter(painter: QtGui.QPainter) -> typing.Generator[None, None, None]: ...
